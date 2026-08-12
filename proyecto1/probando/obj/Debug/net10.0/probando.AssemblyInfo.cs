203aa588e0f31b55585baf135e96bcee5c2a16b9
Juan Díaz <jdiazcol94@gmail.com>
2026-08-12 14:09:53 +0200

    Nuevo paquete agregado

--- proyecto1/probando/obj/Debug/net10.0/probando.AssemblyInfo.cs
+++ proyecto1/probando/obj/Debug/net10.0/probando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("probando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72da6db506e58cdf97ec6c594b79ecd44b22d45d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+168190b650bfc977f2e136dbf901d6bb991e852d")]
 [assembly: System.Reflection.AssemblyProductAttribute("probando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("probando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
